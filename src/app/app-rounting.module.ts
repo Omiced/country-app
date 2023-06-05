2df5923f2e75869839e572d3d94be8290266d388
Josue Cano <73917299+Omiced@users.noreply.github.com>
2023-06-05 23:10:12 +0000

    Creadas paginas home y about

---
+++ src/app/app-rounting.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { Routes } from '@angular/router';
+import { HomePageComponent } from './shared/pages/home-page/home-page.component';
+import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
+
+
+
+const routes: Routes = [
+  {
+    path: "home",
+    component: HomePageComponent,
+  },
+  {
+    path: "about",
+    component: AboutPageComponent,
+  }
+]
+
+@NgModule({
+})
+export class AppRountingModule {
+
+
+}
